7f0dc9a6102df0a9ae6f014c5dac68089d261a28
ianemmanuel <emanu231999@gmail.com>
2024-12-06 14:11:17 +0300

    first commit

---
+++ app/lib/categories.ts
@@ -0,0 +1,22 @@
+export const categories =[
+    {
+        id: 1,
+        title: "Fan Faves",
+        name:"FanFaves"
+    },
+    {
+        id: 2,
+        title: "Veggie",
+        name:"Veggie"
+    },
+    {
+        id: 3,
+        title: "Simple Save",
+        name:"SimpleSave"
+    },
+    {
+        id: 3,
+        title: "New Lunch",
+        name:"NewLunch"
+    }
+]
